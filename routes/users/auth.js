4f106c0ea1def49c4df8baba2d2e55cbc7f7a41a
Alan Chang <alan@alanchang.xyz>
2021-03-24 21:31:59 -0700

    Auth checkin route 1/2 - queries to update users table with checked-in token, and reject if token is invalid

--- routes/users/auth.js
+++ routes/users/auth.js
@@ -16,14 +16,43 @@ accountsRouter.use(express.json());
 accountsRouter.post('/checkin', async (req, res) => {
     try {
         // get the user profile from spotify api;
-        // check if the spotify user id is in our database users table;
-        //  if it is, then set last_checked_in timestamp to now
-        //  if it isn't then add a row with spotify userid, display name, profile picture url, last_checked_in timestamp = now
-        //  add a row to the checkin table corresponding to that user and their last played
+        const { token } = req.body;
+        let validToken = false;
+        let data;
+        data = await axios({
+            method: 'get',
+            url: `https://api.spotify.com/v1/me`,
+            headers: { 
+                'Authorization': `Bearer ${token}`
+            },
+        })
+          .then((response) => {
+            // console.log(response.data);
+            return response.data;
+          })
+          .catch((err) => {
+            return null;
+          });
+        if (data) {
+          console.log(data);
+          let userExists = await pool.query(`SELECT * from users WHERE user_id = '${data.id}'`);
+          userExists = userExists.rows.length !== 0;
+          if (userExists) {
+            await pool.query(`UPDATE users SET last_checked_in = NOW() WHERE user_id = '${data.id}'`)
+          } else {
+            await pool.query(`INSERT INTO users VALUES ('${data.id}', '${data.display_name}', '${data.images[0].url}', NOW())`)
+          }
+          //  TODO: add a row to the checkin table corresponding to that user and their last played 
+          res.send(data);
+        } else {
+          res.status(400).send(false);
+        }
+        
+        
 
         // example of querying db
-        const allUsers = await pool.query('SELECT * FROM users');
-        res.send(allUsers.rows);
+        // const allUsers = await pool.query('SELECT * FROM users');
+        // res.send(allUsers.rows);
         
     } catch (err) {
         res.status(400).send(err.message);
