225874e1e1cb1d4ea44c5aaf3f40d974f12378be
Hang Cao <hmcao1@uci.edu>
2021-03-26 03:09:56 -0700

    route to get friend list and related info

--- routes/users/profile.js
+++ routes/users/profile.js
@@ -19,4 +19,19 @@ profileRouter.post('/', async (req, res) => {
     }
 });
 
+profileRouter.post('/friends', async (req, res) => {
+    try {
+        const friends = await pool.query(`SELECT users.user_id, users.disp_name, users.pfp, last_song.song_name, last_song.song_artist, last_song.img_url FROM 
+        friends INNER JOIN users ON friends.user_id2 = users.user_id
+        INNER JOIN (SELECT * FROM (SELECT user_id, Min(song_id) song
+        FROM (SELECT * FROM checkins ORDER BY checkin_time DESC) recent
+        GROUP BY user_id) user_song
+        INNER JOIN songs ON user_song.song = songs.song_id) AS last_song ON friends.user_id2 = last_song.user_id
+        WHERE user_id1 = '${req.body.user_id}'`);
+        res.send(friends.rows);
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+});
+
 module.exports = profileRouter;
