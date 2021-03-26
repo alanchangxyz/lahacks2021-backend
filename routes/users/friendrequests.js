a67b7cc0504bbf5a63285089c9b18b4fb44f3638
Alan Chang <alan@alanchang.xyz>
2021-03-26 00:24:20 -0700

    Route for getting all friend requests for a user by id

--- routes/users/friendrequests.js
+++ routes/users/friendrequests.js
@@ -6,20 +6,30 @@ const pool = require('../../postgres/config');
 
 friendRequestsRouter.use(express.json());
 
+friendRequestsRouter.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const friendRequests = await pool.query(`SELECT * FROM friend_reqs WHERE user_id2 = '${id}'`);
+        res.send(friendRequests.rows);        
+    } catch(err) {
+        res.status(400).send(err.message);
+    }
+});
+
 friendRequestsRouter.post('/', async (req, res) => {
     try {
         const { user1, user2 } = req.body;
         // make sure both user ids are valid in the db
-        const usersFound = await pool.query(`SELECT FROM users WHERE user_id = '${user1}' OR user_id = '${user2}'`);
+        const usersFound = await pool.query(`SELECT * FROM users WHERE user_id = '${user1}' OR user_id = '${user2}'`);
         if (usersFound.rows.length !== 2) {
             res.send('The user ID you entered is not valid!');
         } else {
             // check if friend relationship exists and no friend request is already pending; create a new one if not exists
-            const relationshipExists = await pool.query(`SELECT from friends WHERE (user_id1 = '${user1}' AND user_id2 = '${user2}') OR (user_id1 = '${user2}' AND user_id2 = '${user1}')`);
+            const relationshipExists = await pool.query(`SELECT * from friends WHERE (user_id1 = '${user1}' AND user_id2 = '${user2}') OR (user_id1 = '${user2}' AND user_id2 = '${user1}')`);
             if (relationshipExists.rows.length > 0) {
                 res.send('You are already friends with that user!');
             } else {
-                const friendReqExists = await pool.query(`SELECT from friend_reqs WHERE (user_id1 = '${user1}' AND user_id2 = '${user2}') OR (user_id1 = '${user2}' AND user_id2 = '${user1}')`);
+                const friendReqExists = await pool.query(`SELECT * from friend_reqs WHERE (user_id1 = '${user1}' AND user_id2 = '${user2}') OR (user_id1 = '${user2}' AND user_id2 = '${user1}')`);
                 if (friendReqExists.rows.length > 0) {
                     res.send('There is already an existing friend request between you and that user!');
                 } else {
