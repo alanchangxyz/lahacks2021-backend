54d32e192ff869b7d8192650d533c45b718b7167
Hang Cao <hmcao1@uci.edu>
2021-03-27 18:37:03 -0700

    set up flask api route for mood model

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request, make_response
+from flask_cors import CORS
+from model import model
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods=["GET"])
+def index():
+    return "Moodacado model backend"
+
+@app.route("/model", methods=["POST"])
+def get_model_pred():
+
+    if request.is_json:
+        try:
+            req = request.get_json()
+            features_list = req.get("audio_features")
+            result_str = model.prediction(features_list)
+            result = json.loads(result_str)
+            return result["0"]
+        except Exception as e:
+            print(e)
+            return "Error converting to csv", 400
+    else:
+        return "Request body must be a JSON object"
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ model/model.py
@@ -0,0 +1,87 @@
+import os
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+import json
+
+# reading and cleaning up the data
+data = pd.read_csv(os.path.join(os.path.dirname(__file__), "../model/data.csv"))
+data = data[data['release_date'] >= '2000']
+data = data.sample(5000)
+
+# keep only some features, song id, song name, and song artist
+songs = data.drop(['duration_ms','explicit','popularity','release_date','year','key','mode','instrumentalness'], axis=1)
+
+# scale loudness and tempo to range from 0 to 1
+# loudness = songs[['loudness']].values
+# min_max_scaler = preprocessing.MinMaxScaler()
+# loudness_scaled = min_max_scaler.fit_transform(loudness)
+# songs['loudness'] = pd.DataFrame(loudness_scaled)
+
+# tempo = songs[['tempo']].values
+# min_max_scaler = preprocessing.MinMaxScaler()
+# tempo_scaled = min_max_scaler.fit_transform(tempo)
+# songs['tempo'] = pd.DataFrame(tempo_scaled)
+
+# drop song id, name, and artist (any any infinity and NaN values)
+features = songs.copy()
+features = features.drop(['id','name','artists'],axis=1)
+
+# by elbow curve, optimal clusters = 4
+kmeans = KMeans(n_clusters=4)
+kmeans.fit(features)
+
+# dimensional reduction to reduce all columns to 2
+y_kmeans = kmeans.predict(features)
+pca = PCA(n_components=2)
+principal_components = pca.fit_transform(features)
+
+# label each song by cluster they belong to
+songs['label'] = y_kmeans
+
+X = features
+y = y_kmeans
+
+# split dataset into two to train and test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+
+# using KNN to classify
+knn = KNeighborsClassifier(n_neighbors=3)
+# Train the model using the training sets
+# knn.fit(X_train,y_train)
+knn.fit(X, y)
+
+knn_pred = knn.predict(X_test)
+
+
+def prediction(song_info):
+    write_to_csv(song_info)
+    song_info_df = pd.read_csv(os.path.join(os.path.dirname(__file__), "../model/song_features.csv"))
+    pred = knn.predict(song_info_df)
+    pred_df = pd.DataFrame(pred)
+    return pred_df.to_json()
+
+
+def write_to_csv(songs):
+    features = ["acousticness", "danceability", "energy","liveness", "loudness", "speechiness", "tempo", "valence"]
+    columnDelimiter = ','
+    lineDelimiter = '\n'
+    result = ''
+    
+    result += columnDelimiter.join(features) + lineDelimiter
+
+    for song in songs:
+        result += columnDelimiter.join([str(song[feature]) for feature in features]) + lineDelimiter
+
+    with open(os.path.join(os.path.dirname(__file__), "../model/song_features.csv"), 'w') as file:
+        file.write(result)
+
+
+
+
+
+
